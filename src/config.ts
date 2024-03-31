4381c40ec695b18f978e43098d3ca86188c9e022
defaultkavy <defaultkavy@gmail.com>
2024-03-31 13:07:37 +0800

    version update

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const config = {
-    version: 'v1.0.3',
+    version: 'v1.0.4',
     github_homepage: 'https://github.com/defaultkavy/youtube-chat-designer',
     author_link: 'https://x.com/defaultkavy'
 }
